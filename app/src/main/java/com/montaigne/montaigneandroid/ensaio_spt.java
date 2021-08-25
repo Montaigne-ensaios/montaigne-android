e6c0cb94f9a4ff20d1e98b4de2c388938e2be016
julia <julia.23js83@gmail.com>
2021-08-25 15:48:42 -0300

    layout de ensaio 1 spt

---
+++ app/src/main/java/com/montaigne/montaigneandroid/ensaio_spt.java
@@ -0,0 +1,14 @@
+package com.montaigne.montaigneandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ensaio_spt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ensaio_spt);
+    }
+}
