176cde60d56dbed73fa928146f19c5640b55d4eb
julia <julia.23js83@gmail.com>
2021-09-01 18:09:51 -0300

    layout edição de ensaio salvo

---
+++ app/src/main/java/com/montaigne/montaigneandroid/layouyt_edicao_ensaios.java
@@ -0,0 +1,14 @@
+package com.montaigne.montaigneandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layouyt_edicao_ensaios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layouyt_edicao_ensaios);
+    }
+}
