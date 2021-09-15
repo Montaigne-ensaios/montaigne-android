59ff9be7202ee414f67ff5457f4d06ab735fe123
julia <julia.23js83@gmail.com>
2021-09-15 17:06:58 -0300

    adicionado tela meus projetos

---
+++ app/src/main/java/com/montaigne/montaigneandroid/meusprojetos.java
@@ -0,0 +1,14 @@
+package com.montaigne.montaigneandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class meusprojetos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meusprojetos);
+    }
+}
