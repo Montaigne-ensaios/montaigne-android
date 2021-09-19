c7ea99bcd33009bf2321224dd6b45b2ce43ecc29
icaro <ibsc013@gmail.com>
2021-09-19 13:21:31 -0300

    Renomeação dos elementos de layout e atribuição da activity SPTCarimbo

--- app/src/main/java/com/montaigne/montaigneandroid/activity/SPTCarimbo.java
+++ app/src/main/java/com/montaigne/montaigneandroid/activity/SPTCarimbo.java
@@ -2,14 +2,35 @@ package com.montaigne.montaigneandroid.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.montaigne.montaigneandroid.R;
 
-public class SPTCarimbo extends AppCompatActivity {
+public class SPTCarimbo extends AppCompatActivity implements View.OnFocusChangeListener {
+    private EditText fieldNome, fieldIni, fieldEmpresa, fieldTel, fieldTecnico, fieldCli,
+            fieldNFuros, fieldLocal;
+    private ImageView imgLogoEmpresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carimbo_spt);
+
+        fieldNome = findViewById(R.id.fieldSPTCarimboNome);
+        fieldIni = findViewById(R.id.fieldSPTCarimboDataIni);
+        fieldEmpresa = findViewById(R.id.fieldSPTCarimboEmpresa);
+        fieldTel = findViewById(R.id.fieldSPTCarimboTel);
+        fieldTecnico = findViewById(R.id.fieldSPTCarimboTecnico);
+        fieldCli = findViewById(R.id.fieldSPTCarimboCli);
+        fieldNFuros = findViewById(R.id.fieldSPTCarimboNFuros);
+        fieldLocal = findViewById(R.id.fieldSPTCarimboNome);
+
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean b) {
+
     }
 }
