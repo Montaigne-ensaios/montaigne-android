7cd0c384f68a4f2c157c7093b78a672e0e2ace54
icaro <ibsc013@gmail.com>
2021-09-19 14:06:33 -0300

    Funcionalidades da activity SPTCarimbo adiconadas

--- app/src/main/java/com/montaigne/montaigneandroid/activity/SPTCarimbo.java
+++ app/src/main/java/com/montaigne/montaigneandroid/activity/SPTCarimbo.java
@@ -1,36 +1,66 @@
 package com.montaigne.montaigneandroid.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.montaigne.montaigneandroid.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SPTCarimbo extends AppCompatActivity implements View.OnFocusChangeListener {
-    private EditText fieldNome, fieldIni, fieldEmpresa, fieldTel, fieldTecnico, fieldCli,
-            fieldNFuros, fieldLocal;
-    private ImageView imgLogoEmpresa;
+    private HashMap<String, EditText> fields = new HashMap<>();
+    private ImageView imgLogoEmpresa, imgSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carimbo_spt);
 
-        fieldNome = findViewById(R.id.fieldSPTCarimboNome);
-        fieldIni = findViewById(R.id.fieldSPTCarimboDataIni);
-        fieldEmpresa = findViewById(R.id.fieldSPTCarimboEmpresa);
-        fieldTel = findViewById(R.id.fieldSPTCarimboTel);
-        fieldTecnico = findViewById(R.id.fieldSPTCarimboTecnico);
-        fieldCli = findViewById(R.id.fieldSPTCarimboCli);
-        fieldNFuros = findViewById(R.id.fieldSPTCarimboNFuros);
-        fieldLocal = findViewById(R.id.fieldSPTCarimboNome);
+        imgLogoEmpresa = findViewById(R.id.imgSPTCarimboImg);
+        imgLogoEmpresa.setOnClickListener(view -> {
+            Toast.makeText(this, "Recurso de imagem precisa ser implementado",
+                    Toast.LENGTH_SHORT).show();
+            // todo: implementar salvamento de imagem
+        });
+
+        imgSalvar = findViewById(R.id.imgSPTCarimboSalvar);
+        imgSalvar.setOnClickListener(view -> {
+            // todo: implementar salvamento ou mudar o caráter deste botão
+            startActivity(new Intent(SPTCarimbo.this, SPTCriar.class));
+            finish();
+            // abre a activity de criação do ensaio e fecha essa
+        });
+        
+        // adiciona cada campo de texto a um hashmap
+        fields.put("Nome", (EditText) findViewById(R.id.fieldSPTCarimboNome));
+        fields.put("Inicio", (EditText) findViewById(R.id.fieldSPTCarimboDataIni));
+        fields.put("Empresa", (EditText) findViewById(R.id.fieldSPTCarimboEmpresa));
+        fields.put("Tel", (EditText) findViewById(R.id.fieldSPTCarimboTel));
+        fields.put("Tecnico", (EditText) findViewById(R.id.fieldSPTCarimboTecnico));
+        fields.put("Clinte", (EditText) findViewById(R.id.fieldSPTCarimboCli));
+        fields.put("NFuros", (EditText) findViewById(R.id.fieldSPTCarimboNFuros));
+        fields.put("Local", (EditText) findViewById(R.id.fieldSPTCarimboNome));
+
+        for(Map.Entry<String, EditText> set : fields.entrySet()){
+            set.getValue().setOnFocusChangeListener(this);
+        }
+        // adiciona um listener para cada field
 
     }
 
     @Override
     public void onFocusChange(View view, boolean b) {
-
+        if(!b) {
+            // todo: implementar funções da base de dados para salvamentos
+            Toast.makeText(this, "Desfoque de View detectado",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
