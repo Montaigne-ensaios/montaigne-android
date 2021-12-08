da8b655dc16253f7e9e0193d144962f555514107
icaro <ibsc013@gmail.com>
2021-12-08 11:35:46 -0300

    Criação da activity_sobre.xml e Sobre.java

---
+++ app/src/main/java/com/montaigne/montaigneandroid/activity/Sobre.java
@@ -0,0 +1,16 @@
+package com.montaigne.montaigneandroid.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.montaigne.montaigneandroid.R;
+
+public class Sobre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
